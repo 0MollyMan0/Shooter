ecd3dc257aa0e505ba1fe9fdb453ce661c462e08
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-08 21:04:04 +0100

    add class game and restructuration for clean code by separating class by files

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import pygame
+from game import Game
+
 pygame.init()
-
-class Player(pygame.sprite.Sprite):
-
-    def __init__(self):
-        super().__init__()
-        self.health = 100
-        self.max_health = 100
-        self.attack = 10
-        self.speed = 5
-        self.image = pygame.image.load('./assets/player.png')
-        self.rect = self.image.get_rect()
-        self.rect.x = 400
-        self.rect.y = 500
 
 pygame.display.set_caption("Shooter") # add a custom icon here in the future
 screen = pygame.display.set_mode((1080, 720))
 
 background = pygame.image.load('assets/bg.jpg')
 
-player = Player()
+game = Game()
 
 running = True
 
@@ -27,7 +16,7 @@ while running:
 
     screen.blit(background, (0, -200))
 
-    screen.blit(player.image, player.rect)
+    screen.blit(game.player.image, game.player.rect)
 
     pygame.display.flip()
 
