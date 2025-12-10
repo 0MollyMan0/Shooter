9124f411e55146ff3926019190f6475e166e7f47
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-10 20:26:19 +0100

    projectile rotate

--- projetctile.py
+++ projetctile.py
@@ -11,11 +11,20 @@ class Projectile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 125
         self.rect.y = player.rect.y + 80
+        self.origin_image = self.image
+        self.angle = 0
+
+    def rotate(self):
+        self.angle += 2
+        self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
 
     def remove(self):
         self.player.all_projectiles.remove(self)
 
     def move(self):
         self.rect.x += self.velocity
+        self.rotate()
         if self.rect.x > 1080:
             self.remove()
